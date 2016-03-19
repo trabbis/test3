996314768045447235f29f4da23702b1c66a1739
trabbis <travis.choi@gmail.com>
2016-03-19 12:10:26 -0400

    added java doc testing

---
+++ src/main/java/com/java/core/JavaDocTesting.java
@@ -0,0 +1,35 @@
+package com.java.core;
+
+import org.omg.PortableServer.POAPackage.AdapterAlreadyExistsHelper;
+
+/**
+ * 
+ * @author a
+ * {@link AdapterAlreadyExistsHelper}
+ * {@link String#charAt(int)}
+ * @throws 
+ */
+public class JavaDocTesting {
+
+	
+	/**
+	 * 
+	 * @param arg1
+	 * @return
+	 * @throws
+	 */
+	public String testing1(String arg1) {
+	
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param arg2
+	 * @return
+	 */
+	public Integer testing2(String arg2) {
+		return null;
+		
+	}
+}
