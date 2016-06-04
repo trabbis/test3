8bf9893773d3fe294f03f41c81395fb2344f3ceb
trabbis <travis.choi@gmail.com>
2016-06-04 15:13:32 -0400

    default accessor variable is only accessible within same package

--- src/main/java/com/java/core/accessorB/AccessorDemo2.java
+++ src/main/java/com/java/core/accessorB/AccessorDemo2.java
@@ -6,7 +6,7 @@ public class AccessorDemo2 extends AccessorA{
 	
 	public AccessorDemo2() {
 //		String test1 = privateStr; // private no way. you can not access at all
-//		String test2 = defaultStr; //Default you can access within same package
+//		String test2 = defaultStr; //Default you can access only within same package
 		String test3 = protectedStr; //Protected variable, you can access as long as you subclass
 		
 		
