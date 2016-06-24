b4b3f7e5eae37c26bd5f1168f53aa52b7ceceef9
dchoi <dchoi@PS-DCHOI.nbspaymentsolutions.com>
2016-06-24 13:36:50 -0400

    can you extends abstract class in the interface?

---
+++ src/main/java/com/java/core/AbstractTest1.java
@@ -0,0 +1,17 @@
+/****************************************************************************************
+ * $RCSfile: eclipse-codetemplates.xml,v $  $Revision: 1.4 $  $Date: 2014/10/10 20:48:18 $
+ * Copyright 2008,2014 All rights reserved. NBS Payment Solutions Inc.
+ *
+ ****************************************************************************************/
+
+package com.java.core;
+
+/**
+ * @author dchoi@nbsps.com
+ *
+ */
+public class AbstractTest1 {
+    private String user;
+    private String eventActionType;
+    
+}
---
+++ src/main/java/com/java/core/InterfaceTest1.java
@@ -0,0 +1,19 @@
+/****************************************************************************************
+ * $RCSfile: eclipse-codetemplates.xml,v $  $Revision: 1.4 $  $Date: 2014/10/10 20:48:18 $
+ * Copyright 2008,2014 All rights reserved. NBS Payment Solutions Inc.
+ *
+ ****************************************************************************************/
+
+package com.java.core;
+
+/**
+ * @author dchoi@nbsps.com
+ *
+ */
+public interface InterfaceTest1 { //extends AbstractTest1 {
+    //The type AbstractTest1 cannot be a superinterface of InterfaceTest1; 
+    //a superinterface must be an interfac
+    //==> interface can extend from superinterface only
+    
+ 
+}
