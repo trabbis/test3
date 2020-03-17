7b077704179a4db87f77467a1f394bfe1582c38f
Duksun Choi <duksun.choi@telus.com>
2020-03-17 17:12:20 -0400

    added for http connection test

---
+++ src/main/java/com/travis/Tester.java
@@ -0,0 +1,101 @@
+package com.travis;
+
+import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
+import java.net.Proxy;
+import java.net.URL;
+import java.security.cert.Certificate;
+import java.io.*;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLPeerUnverifiedException;
+
+public class Tester{
+	
+   public static void main(String[] args)
+   {
+        new Tester().testIt();
+   }
+	
+   private void testIt(){
+
+      String https_url = "https://apigw-st.telus.com/dv/jwks";
+      URL url;
+      try {
+    	  
+    	  Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("198.161.14.25", 8080));
+
+	     url = new URL(https_url);
+	     HttpsURLConnection con = (HttpsURLConnection)url.openConnection(proxy);
+			
+	     //dumpl all cert info
+	     print_https_cert(con);
+			
+	     //dump all the content
+	     print_content(con);
+			
+      } catch (MalformedURLException e) {
+	     e.printStackTrace();
+      } catch (IOException e) {
+	     e.printStackTrace();
+      }
+
+   }
+	
+   private void print_https_cert(HttpsURLConnection con){
+     
+    if(con!=null){
+			
+      try {
+				
+	System.out.println("Response Code : " + con.getResponseCode());
+	System.out.println("Cipher Suite : " + con.getCipherSuite());
+	System.out.println("\n");
+				
+	Certificate[] certs = con.getServerCertificates();
+	for(Certificate cert : certs){
+	   System.out.println("Cert Type : " + cert.getType());
+	   System.out.println("Cert Hash Code : " + cert.hashCode());
+	   System.out.println("Cert Public Key Algorithm : " 
+                                    + cert.getPublicKey().getAlgorithm());
+	   System.out.println("Cert Public Key Format : " 
+                                    + cert.getPublicKey().getFormat());
+	   System.out.println("\n");
+	}
+				
+	} catch (SSLPeerUnverifiedException e) {
+		e.printStackTrace();
+	} catch (IOException e){
+		e.printStackTrace();
+	}
+
+     }
+	
+   }
+	
+   private void print_content(HttpsURLConnection con){
+	if(con!=null){
+			
+	try {
+		
+	   System.out.println("****** Content of the URL ********");			
+	   BufferedReader br = 
+		new BufferedReader(
+			new InputStreamReader(con.getInputStream()));
+				
+	   String input;
+				
+	   while ((input = br.readLine()) != null){
+	      System.out.println(input);
+	   }
+	   br.close();
+				
+	} catch (IOException e) {
+	   e.printStackTrace();
+	}
+			
+       }
+		
+   }
+	
+}
