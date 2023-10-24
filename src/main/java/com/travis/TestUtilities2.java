4d943d56978c82158117dc9bf07c18e81f911820
choid <choid@WL005204.tci.internal.toyota.ca>
2023-10-24 16:28:34 -0400

    testing

--- src/main/java/com/travis/TestUtilities2.java
+++ src/main/java/com/travis/TestUtilities2.java
@@ -8,11 +8,18 @@ import java.util.Date;
 public class TestUtilities2 {
 
 	public static void main (String argv[]) throws Exception {
-		convertLocalDateToDate();
+		TestUtilities2 testUtilities2 = new TestUtilities2();
+		//testUtilities2.convertLocalDateToDate();
+
+//		connectorProps.put("sharePointUrl", "https://myteams.toyota.com/sites/TCIPRMS-UAT/_api/web/GetFolderByServerRelativeUrl('/sites/TCIPRMS-UAT/Shared%20Documents/')/Files"); //Which url to use?
+		String sharePointDomainUrl = "https://myteams.toyota.com/";
+		String appName = "TCIPRMS-UAT";
+		String folderName = testUtilities2.getFolderPath(sharePointDomainUrl, appName);
+		System.out.println(folderName);
 	}
 	
 	
-	private static void convertLocalDateToDate() {
+	private void convertLocalDateToDate() {
 
 		ZoneId defaultZoneId = ZoneId.systemDefault();
 		LocalDate localDate = LocalDate.now();
@@ -25,5 +32,24 @@ public class TestUtilities2 {
 	}
 
 
+	
+	public String getFolderPath(String sharePointDomainUrl, String appName) {
+		final String SHAREPOINT_RELATIVE_PATH = "%s/_api/web/GetFolderByServerRelativeUrl('%s')/Files";
+//		String ADD_FILE_COMMAND = "%s/_api/web/GetFolderByServerRelativeUrl('%s')/Files/add(url='%s',overwrite=true)";
+
+		final String SHARED_DOCUMENT_PATH = "/sites/%s/Shared Documents/";
+
+		String rootUrl = String.format("%ssites/%s", sharePointDomainUrl, appName);
+	 	    //return String.format("%ssites/%s/", this.sharepointInfo.get_sharepointRootUrl(), request.getAppName());
+		
+		String folderPath = encodeSpaceInString(String.format(SHARED_DOCUMENT_PATH, appName));
+		return String.format(SHAREPOINT_RELATIVE_PATH, rootUrl, folderPath);
+	}
+	
+	private String encodeSpaceInString(String s){
+		String ENCODED_SPACE = "%20";
+		return s.replaceAll(" ", ENCODED_SPACE);
+	}
+	
 	
 }
