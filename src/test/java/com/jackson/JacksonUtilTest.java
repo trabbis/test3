f49df8cf0e2816f650846afdc1b681550b0f35b4
choid <choid@WL005204.tci.internal.toyota.ca>
2023-10-24 16:27:55 -0400

    jackson library testing

--- src/test/java/com/jackson/JacksonUtilTest.java
+++ src/test/java/com/jackson/JacksonUtilTest.java
@@ -5,11 +5,27 @@ import static org.hamcrest.Matchers.containsString;
 
 import org.junit.jupiter.api.Test;
 
+import com.fasterxml.jackson.annotation.JsonRawValue;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JacksonUtilTest {
 	
+	@Test
+	public void whenSerializingWithoutJsonRawValue()
+	  throws JsonProcessingException {
+	 
+		RawBeanWithoutJsonRawValue bean = new RawBeanWithoutJsonRawValue("My bean", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+
+	    String result = new ObjectMapper().writeValueAsString(bean);
+	    System.out.println(result);
+	    assertThat(result, containsString("My bean"));
+	    assertThat(result, containsString("<?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?>"));
+	}
+	
+	//Tesing @JsonRawValue
+	//Bit tricky to understand fully, but basically @JsonRawValue annotation removes any escaping characters
+	//So when we serialize regular bean with @JsonRawValue annotation, it will remove any escape characters
 	@Test
 	public void whenSerializingUsingJsonRawValue_thenCorrect()
 	  throws JsonProcessingException {
@@ -18,23 +34,12 @@ public class JacksonUtilTest {
 	    
 
 	    String result = new ObjectMapper().writeValueAsString(bean);
+	    System.out.println("with jsonrawvalue..." + result);
 	    
 	    assertThat(result, containsString("My bean"));
 	    assertThat(result, containsString("{\"attr\":\"value\"}"));
 	}
 
-	//Bit tricky to understand fully, but basically @JsonRawValue annotation removes any escaping characters
-	//So if we use regular bean with @JsonRawValue annotation, when we serialize, it will contains escape characters
-	@Test
-	public void whenSerializingWithoutJsonRawValue()
-	  throws JsonProcessingException {
-	 
-		RawBeanWithoutJsonRawValue bean = new RawBeanWithoutJsonRawValue("My bean", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-
-	    String result = new ObjectMapper().writeValueAsString(bean);
-	    assertThat(result, containsString("My bean"));
-	    assertThat(result, containsString("<?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?>"));
-	}
 
 	
 }
