c360f8b5d5eaaf46ebbfaabf77280c1e1e7055e2
dchoi <dchoi@LTS2104.staff.local>
2024-10-03 14:31:09 -0400

    refactoring using StringBuilder instead of string concatenation

--- src/main/java/com/travis/TestUtilities3.java
+++ src/main/java/com/travis/TestUtilities3.java
@@ -4,7 +4,10 @@ public class TestUtilities3 {
 	
 	public static String capitalizeIfNttParam(String paramName, String paramValue) {
 		if (paramName != null && paramName.equalsIgnoreCase("ntt") && paramValue != null && paramValue.length() >= 1) {
-			return paramValue.substring(0, 1).toUpperCase() + paramValue.substring(1);
+			StringBuilder sb = new StringBuilder();
+			sb.append(paramValue.substring(0, 1).toUpperCase());
+			sb.append(paramValue.substring(1));
+			return sb.toString();
 		}
 		return paramValue;
 	}
